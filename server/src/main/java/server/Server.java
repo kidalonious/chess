f9543db2e574fe57d7b37180ef1e89e98503c23a
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-02-22 17:19:17 -0700

    Server is running, not working, but runs

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,7 +10,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.delete("/db", (req, res) -> null);
         Spark.awaitInitialization();
         return Spark.port();
     }

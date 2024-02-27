4436069575834ca53d6cbb26a1ac2b94f14072b2
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-02-27 15:29:24 -0700

    Tried to implement error handling for logout. It still doesn't work

--- server/src/main/java/server/handlers/LogoutHandler.java
+++ server/src/main/java/server/handlers/LogoutHandler.java
@@ -1,12 +1,24 @@
 package server.handlers;
 
+import com.google.gson.Gson;
 import server.services.LogoutService;
 import spark.Request;
 import spark.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LogoutHandler {
     public static Object handle(Request request, Response response) {
-        String authToken = request.headers("authorization");
+        String authToken = request.headers("Authorization");
+        String userName = request.headers().toString();
+        System.out.println(userName);
+        if (authToken == null) {
+            response.status(401);
+            Map<String, String> attributeMap = new HashMap<>();
+            attributeMap.put("message", "Error: unauthorized");
+            return new Gson().toJson(attributeMap);
+        }
         LogoutService.logout(authToken);
         response.status(200);
         return "{}";

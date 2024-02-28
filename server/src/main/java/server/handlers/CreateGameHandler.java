c18cf463bef7cb37577e8a04fb2f056bfd772fad
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-02-27 21:01:17 -0700

    Succesfully implemented CreateGame Exception handling

--- server/src/main/java/server/handlers/CreateGameHandler.java
+++ server/src/main/java/server/handlers/CreateGameHandler.java
@@ -1,6 +1,8 @@
 package server.handlers;
 
 import com.google.gson.Gson;
+import dataAccess.BadRequestException;
+import dataAccess.UnauthorizedException;
 import model.GameData;
 import server.requests.GameRequest;
 import server.services.CreateGameService;
@@ -12,13 +14,33 @@ import java.util.Map;
 
 public class CreateGameHandler {
     public static Object handle(Request request, Response response) {
-        String usableRequest = GameRequest.convertToString(request);
-        GameData newGame = GameRequest.gson.fromJson(usableRequest, GameData.class);
-        int gameID = CreateGameService.createGame(newGame);
-        response.status(200);
-        Map<String, Integer> attributeMap = new HashMap<>();
-        attributeMap.put("gameID", gameID);
-        Gson gson = new Gson();
-        return gson.toJson(attributeMap);
+        try {
+            String usableRequest = GameRequest.convertToString(request);
+            GameData newGame = GameRequest.gson.fromJson(usableRequest, GameData.class);
+            int gameID = CreateGameService.createGame(newGame, request);
+            response.status(200);
+            Map<String, Integer> attributeMap = new HashMap<>();
+            attributeMap.put("gameID", gameID);
+            Gson gson = new Gson();
+            return gson.toJson(attributeMap);
+        }
+        catch (UnauthorizedException e) {
+            response.status(401);
+            Map<String, String> errorMessage = new HashMap<>();
+            errorMessage.put("message", "Error: " + e.getMessage());
+            return new Gson().toJson(errorMessage);
+        }
+        catch (BadRequestException e) {
+            response.status(400);
+            Map<String, String> errorMessage = new HashMap<>();
+            errorMessage.put("message", "Error: " + e.getMessage());
+            return new Gson().toJson(errorMessage);
+        }
+        catch (Exception e) {
+            response.status(500);
+            Map<String, String> errorMessage = new HashMap<>();
+            errorMessage.put("message", "Error: " + e.getMessage());
+            return new Gson().toJson(errorMessage);
+        }
     }
 }
--- server/src/main/java/server/services/CreateGameService.java
+++ server/src/main/java/server/services/CreateGameService.java
@@ -1,10 +1,19 @@
 package server.services;
 
+import dataAccess.BadRequestException;
 import dataAccess.GameMemoryDAO;
+import dataAccess.UnauthorizedException;
 import model.GameData;
+import spark.Request;
 
 public class CreateGameService extends Service{
-    public static int createGame(GameData newGame) {
+    public static int createGame(GameData newGame, Request request) throws Exception{
+        if (newGame.gameName() == null) {
+            throw new BadRequestException("bad request");
+        }
+        if (authMemoryDAO.getAuthData(request.headers("Authorization")) == null) {
+            throw new UnauthorizedException("unauthorized");
+        }
 
         return gameMemoryDAO.createGame(newGame);
     }

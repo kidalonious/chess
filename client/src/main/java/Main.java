593096978a2f3cf7f304d82a4ac2ae3859a9bb9b
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-03-21 19:57:58 -0600

    Implemented the Repl with logged in client and logged out client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,36 @@
 import chess.*;
+import dataAccess.*;
+import server.*;
+import ui.Repl;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        var serverUrl = "http://localhost:8080";
+        if (args.length == 1)
+        {
+            serverUrl = args[0];
+        }
+
+        Server chessServer = new Server();
+        chessServer.run(8080);
+
+        try{
+            AuthDAO authDAO = new SQLAuthDAO();
+            authDAO.clear();
+            GameDAO gameDAO = new SQLGameDAO();
+            gameDAO.clear();
+            UserDAO userDAO = new SQLUserDAO();
+            userDAO.clear();
+        }
+        catch(Exception ex)
+        {
+            System.exit(0);
+        }
+
+
+
+        new Repl(serverUrl).run();
     }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Repl {
@@ -15,12 +16,46 @@ public class Repl {
         authToken = newAuth;
     }
 
+    public Repl(String serverURL) {
+        loggedOutClient = new LoggedOutClient(serverURL);
+        loggedInClient = new LoggedInClient(serverURL);
+    }
+
     public void run() {
         System.out.println("Welcome to Chess!");
         Scanner scanner = new Scanner(System.in);
+        var method = "";
         var userInput = "";
-        while (userInput != "quit") {
-
+        System.out.println(loggedOutClient.help());
+        while (!Objects.equals(userInput, "quit")) {
+            if (state == States.LOGGEDOUT) {
+                //BaseClient.print();
+                userInput = scanner.nextLine();
+                try {
+                    method = loggedOutClient.eval(userInput);
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_GREEN + method + "\n");
+                }
+                catch (Throwable e) {
+                    var message = e.toString();
+                    System.out.print(message);
+                }
+                System.out.println();
+            }
+            if (state == States.LOGGEDIN) {
+                System.out.println(loggedInClient.help());
+                //BaseClient.print();
+                userInput = scanner.nextLine();
+                try {
+                    method = loggedInClient.eval(userInput);
+                    System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE + method + "\n");
+                }
+                catch (Throwable e) {
+                    var message = e.toString();
+                    System.out.print(message);
+                }
+                System.out.println();
+            }
         }
+        System.exit(0);
     }
 }

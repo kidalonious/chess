051ec72b5a6e5dcc94723c305aab85f3c9e15d81
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-04-08 13:39:34 -0600

    small refactor

--- server/src/main/java/server/webSocket/WebSocketHandler.java
+++ server/src/main/java/server/webSocket/WebSocketHandler.java
@@ -1,4 +1,19 @@
 package server.webSocket;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.io.IOException;
+import java.util.Timer;
+
+@WebSocket
 public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+    }
 }

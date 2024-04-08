6d3eae5b6aaf0b57863e9654a59fe28026324e10
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-04-08 14:18:50 -0600

    put cases and dummy functions into WebSocketHandler for UserGameCommands per each case

--- server/src/main/java/server/webSocket/WebSocketHandler.java
+++ server/src/main/java/server/webSocket/WebSocketHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import webSocketMessages.userCommands.UserGameCommand;
 
 import java.io.IOException;
 import java.util.Timer;
@@ -15,5 +16,30 @@ public class WebSocketHandler {
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
+        switch (userGameCommand.getCommandType()) {
+            case JOIN_PLAYER -> join_player();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+            case MAKE_MOVE -> make_move();
+            case JOIN_OBSERVER -> join_observer();
+        }
+    }
+
+    public void join_player() {
+
+    }
+
+    public void join_observer() {
+
+    }
+    public void make_move() {
+
+    }
+    public void leave() {
+
+    }
+    public void resign() {
+
     }
 }

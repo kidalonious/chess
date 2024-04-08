b4ae2c31ef32d58cac1f875333998e7f776d4411
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-04-08 13:28:27 -0600

    copied some websocket code from petshop

---
+++ client/src/main/java/ui/webSocket/WebSocketFacade.java
@@ -0,0 +1,46 @@
+package ui.webSocket;
+
+import com.google.gson.Gson;
+import dataAccess.DataAccessException;
+import webSocketMessages.Action;
+import webSocketMessages.Notification;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+//need to extend Endpoint for websocket to work properly
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws Exception {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/connect");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+    //Endpoint requires this method, but you don't have to do anything
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }

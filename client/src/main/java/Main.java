aded9dfd76fd8c010c652e0fe399590d9e1c4a3b
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-04-11 13:27:07 -0600

    uncommented the server startup

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -7,16 +7,16 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-        //Server server = new Server();
-        //server.run(8080);
+        Server server = new Server();
+        server.run(8080);
         var serverUrl = "http://localhost:8080";
         if (args.length == 1) {
             serverUrl = args[0];
         }
         try{
-//            AuthDAO authDAO = new SQLAuthDAO();
-//            GameDAO gameDAO = new SQLGameDAO();
-//            UserDAO userDAO = new SQLUserDAO();
+            AuthDAO authDAO = new SQLAuthDAO();
+            GameDAO gameDAO = new SQLGameDAO();
+            UserDAO userDAO = new SQLUserDAO();
 
 
             new Repl(serverUrl).run();
